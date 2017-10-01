506e0093f5c49fcd5b7e3bb0b61f3d2c61e5ec7f
Samuel Biondolillo <biwinning121@gmail.com>
2017-10-01 08:41:58 -0400

    create TextFileProcessor class

---
+++ src/classes/TextFileProcessor.java
@@ -0,0 +1,13 @@
+/*
+ * TextFileProcessor Class
+ * Samuel Biondolillo
+ * CIS210M - Data Structures and Elementary Algorithms
+ * Goal - build a class to turn a text file into a list of strings
+ * Version	0.0.1	10/1/17
+ */
+
+package classes;
+
+public class TextFileProcessor {
+
+}
