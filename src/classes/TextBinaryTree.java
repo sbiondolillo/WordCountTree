aae607d09cff946b428be7e81bdb8aebf7b2c711
Samuel Biondolillo <biwinning121@gmail.com>
2017-10-01 16:18:30 -0400

    create TextBinaryTree

---
+++ src/classes/TextBinaryTree.java
@@ -0,0 +1,15 @@
+/*
+ * TextBinaryTree Class
+ * Samuel Biondolillo
+ * CIS210M:ZZ Data Structures and Elementary Algorithms
+ * Goal: To create a special class which models a Binary Search Tree with 
+ * TextNodes which hold an ArrayList<String>
+ * Version  0.0.1   10/1/17
+ */
+
+package classes;
+
+public class TextBinaryTree extends BinaryTree {
+
+	
+}
