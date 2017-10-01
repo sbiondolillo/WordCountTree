8246f93a74ed8788c11ee0fec40effee07e76e77
Samuel Biondolillo <biwinning121@gmail.com>
2017-10-01 09:08:28 -0400

    create TestWordCountTree class

---
+++ src/classes/TestWordCountTree.java
@@ -0,0 +1,18 @@
+/*
+ * TestWordCountTree Class
+ * Samuel Biondolillo
+ * CIS210M - Data Structures and Elementary Algorithms
+ * Goal - build a class to test our program
+ * Version	0.0.1	10/1/17
+ */
+
+package classes;
+
+public class TestWordCountTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
