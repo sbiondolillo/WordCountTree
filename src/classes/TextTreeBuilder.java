4da57b6eee97569ad72bfcf3c8e58c6e54564693
Samuel Biondolillo <biwinning121@gmail.com>
2017-10-01 10:52:25 -0400

    create TextTreeBuilder class

---
+++ src/classes/TextTreeBuilder.java
@@ -0,0 +1,13 @@
+/*
+ * TextTreeBuilder Class
+ * Samuel Biondolillo
+ * CIS210M - Data Structures and Elementary Algorithms
+ * Goal - create a class to build a tree of TextNodes from a HashMap<String,Integer>
+ * Version 	0.0.1	10/1/17
+ */
+
+package classes;
+
+public class TextTreeBuilder {
+
+}
